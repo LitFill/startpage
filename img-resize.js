baaf328773946250562e9c564c5241c4fee2a1f1
LitFill <98679748+LitFill@users.noreply.github.com>
2023-07-07 15:40:37 +0700

    Update img-resize.js

--- img-resize.js
+++ img-resize.js
@@ -1,1 +1,8 @@
 document.getElementById("image").style.height = document.getElementById("links").offsetHeight + "px";
+
+const linksElement = document.getElementById("links");
+const linksHeight = linksElement.offsetHeight;
+
+console.log(linksHeight);
+
+document.querySelector('.refresh').addEventListener('click', () => location.reload());
