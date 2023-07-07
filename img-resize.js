c4ad96d44e14867558927939b89d325e5650d78b
LitFill <98679748+LitFill@users.noreply.github.com>
2023-07-07 16:40:27 +0700

    fix img

--- img-resize.js
+++ img-resize.js
@@ -1,8 +1,7 @@
 document.getElementById("image").style.height = document.getElementById("links").offsetHeight + "px";
-
-const linksElement = document.getElementById("links");
-const linksHeight = linksElement.offsetHeight;
-
-console.log(linksHeight);
-
 document.querySelector('.refresh').addEventListener('click', () => location.reload());
+
+// const linksElement = document.getElementById("links");
+// const linksHeight = linksElement.offsetHeight;
+
+// console.log(linksHeight);
