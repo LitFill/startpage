f3e75ccb3f2a1716f9aa8cdcc5e832a43c040fef
LitFill <98679748+LitFill@users.noreply.github.com>
2023-07-26 13:37:11 +0700

    update startpage to add some google's and a footer

--- startpage/README.md
+++ startpage/README.md
@@ -4,3 +4,5 @@ Also works as a Chrome extension!
 It even has dark mode!
 
 ![Showcase](repo-assets/preview-2.png)
+
+## Repo untuk startpage pribadi milikku yang ku fork dari startpage milik orang
--- startpage/img-resize.js
+++ startpage/img-resize.js
@@ -1,8 +1,11 @@
-document.getElementById("image").style.height = document.getElementById("links").offsetHeight + "px";
+document.getElementById("image").style.height =
+    document.getElementById("links").offsetHeight + "px";
 
 const linksElement = document.getElementById("links");
 const linksHeight = linksElement.offsetHeight;
 
 console.log(linksHeight);
 
-document.querySelector('.refresh').addEventListener('click', () => location.reload());
+document
+    .querySelector(".refresh")
+    .addEventListener("click", () => location.reload());
