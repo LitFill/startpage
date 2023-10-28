66542fd70cf8b8f01eff8eb17c6564449697f5e8
LitFill <98679748+LitFill@users.noreply.github.com>
2023-10-28 16:18:27 +0700

    📃 docs: jsdoc
    Add: JSDoc to img-resize.js
    for better code documentation and understanding

--- img-resize.js
+++ img-resize.js
@@ -1,7 +1,23 @@
+/**
+ * Set the height of the image equal to the offsetHeight of the links element
+ * @type {string}
+ */
 document.getElementById("image").style.height =
     document.getElementById("links").offsetHeight + "px";
 
+/**
+ * Elements with the .links class
+ * @type {HTMLElement}
+ */
 const linksElement = document.getElementById("links");
+
+/**
+ * The height of the linksElement
+ * @type {number}
+ */
 const linksHeight = linksElement.offsetHeight;
 
+/**
+ * Logs the height of the linksElement in the console
+ */
 console.log(linksHeight);
